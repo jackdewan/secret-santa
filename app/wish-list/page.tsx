c8e324f0d4f653c5feb0728a013cb84b5fc081be
Jack <jdewan16@gmail.com>
2023-12-13 21:17:17 -0500

    add ogs error handling

--- app/wish-list/page.tsx
+++ app/wish-list/page.tsx
@@ -32,29 +32,38 @@ export default async function WishListPage() {
 
     const href = formData.get("href") as string;
 
-    const ogs = require("open-graph-scraper");
-    const userAgent =
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
-    const options = {
-      url: href,
-      fetchOptions: { headers: { "user-agent": userAgent } },
-    };
-    const ogsData = await ogs(options);
+    let insertHref, insertTitle;
 
-    const { error: ogsErr, html, result, response } = ogsData;
+    try {
+      const ogs = require("open-graph-scraper");
+      const userAgent =
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
+      const options = {
+        url: href,
+        fetchOptions: { headers: { "user-agent": userAgent } },
+      };
+      const ogsData = await ogs(options);
 
-    console.log("error:", ogsErr); // This returns true or false. True if there was an error. The error itself is inside the result object.
-    console.log("result:", result); // This contains all of the Open Graph results
+      const { error: ogsErr, html, result, response } = ogsData;
 
+      insertHref = result.ogUrl;
+      insertTitle = result.ogTitle;
+    } catch (error: any) {
+      insertHref = null;
+      insertTitle = error.result.requestUrl;
+    }
+
+    // console.log("error------:", ogsErr); // This returns true or false. True if there was an error. The error itself is inside the result object.
+    // console.log("result-----:", result); // This contains all of the Open Graph results
+
+    console.log(insertTitle);
     const { data, error } = await supabase
       .from("links")
-      .insert({ href: result.ogUrl, title: result.ogTitle, user_id: user.id });
+      .insert({ href: insertHref, title: insertTitle, user_id: user.id });
 
     if (error) {
       console.log("error: ", error);
     }
-
-    console.log("data", data);
 
     return revalidatePath("/");
   };
